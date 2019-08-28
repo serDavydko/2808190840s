e9bbcf1805b60f482bf2780c560f402d0bc585fd
Serhii <davydko.serhiy@gmail.com>
2019-08-28 15:10:52 +0300

    Updates

---
+++ precache-manifest.fba55351a9f7e51aaa709416c71aeb8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2615e27d989f790640ebe3bb68cee81",
+    "url": "/2808190840s/index.html"
+  },
+  {
+    "revision": "23f82b504b053cb6447b",
+    "url": "/2808190840s/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/2808190840s/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "23f82b504b053cb6447b",
+    "url": "/2808190840s/static/js/main.1403a70d.chunk.js"
+  },
+  {
+    "revision": "97309f30d34130ff966b",
+    "url": "/2808190840s/static/js/runtime~main.38b54574.js"
+  }
+]);
